3e7011fed7a10a94e797a2b6a57c5ae1e7acc76e
Drew Crawford <drew@sealedabstract.com>
2014-01-03 08:29:04 -0600

    tests

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ from teamcity.unittestpy import TeamcityTestRunner
 import unittest
 if __name__ == '__main__':
     import sys
+    print sys.argv
     if "--teamcity" in sys.argv:
         runner = TeamcityTestRunner()
     else:
