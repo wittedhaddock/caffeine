315eae8568b5b8ae9839c1a35bbfba21cfe9f59e
Drew Crawford <drew@sealedabstract.com>
2014-01-18 16:52:39 -0600

    forgot pack.py

---
+++ pack.py
@@ -0,0 +1,57 @@
+# install a bunch of pack behavior on default objects
+
+# This is somewhat clumsy because we cannot install methods on default types
+
+import security
+
+
+def findPackMethod(obj):
+    if hasattr(obj, "_caffeinePack"):
+        return obj._caffeinePack
+    elif isinstance(obj, list):
+        return listPack
+    elif isinstance(obj, dict):
+        return dictPack
+    elif isinstance(obj, int):
+        return lambda x: obj
+    elif isinstance(obj, str):
+        return lambda x: obj
+    else:
+        raise ValueError("Don't know how to pack %s" % obj.__class__)
+
+
+def findUnpackMethod(packet):
+        # Security warning: be careful when adding new methods here.  We assume
+        # they're safe.
+    if isinstance(packet, str):
+        return lambda x: x
+
+    print (packet,"packet was")
+    klassname = packet["_c"]
+    klass = security.string_to_class(klassname)
+    if hasattr(klass, "_caffeineUnpack"):
+        return klass._caffeineUnpack
+    elif klassname=="dict":
+        return dictUnpack
+    else:
+        raise ValueError("Don't know how to unpack %s" % klassname)
+
+
+def pack(obj):
+    return findPackMethod(obj)(obj)
+
+
+def unpack(packet):
+    method = findUnpackMethod(packet)
+    return method(packet)
+
+
+def listPack(lyst):
+    return {"_c": "list", "list": [pack(item) for item in lyst]}
+
+
+def dictPack(dikt):
+    return {"_c": "dict", "dict": {pack(key): pack(value) for key, value in dikt.items()}}
+
+def dictUnpack(dikt):
+    return {unpack(key):unpack(value) for key,value in dikt["dict"].items()}
