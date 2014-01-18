ba333f1e7fa6f5c27efdc910ae19fb178df09d4e
Drew Crawford <drew@sealedabstract.com>
2014-01-18 13:24:47 -0600

    basic schema support

--- RPC.py
+++ RPC.py
@@ -1,5 +1,8 @@
 root_level_objects = {}
 
+
+
+# This sweeps marked functions for RPC access.
 def CaffeineClass(klass):
     root_level_objects[klass.__name__]=klass
     klass._caffeineRPC = {}
@@ -8,8 +11,36 @@ def CaffeineClass(klass):
             klass._caffeineRPC[name] = method
     return klass
 
+#We use a mark and sweep pattern.  The class doesn't exist at the time we create the function, so we just mark.  The sweep occurs in CaffeineClass.
 def CaffeinePublicMethod(method):
     method._caffeineRPC = True
     return method
+
+
+class Schema:
+    def __init__(self,klass):
+        self.klass = klass
+
+    def _caffeinePack(self):
+        d = {}
+        d["_c"]="Schema"
+        d["functions"] = {}
+        for name,method in self.klass.__dict__.items():
+            if hasattr(method,"_caffeineRPC"):
+                d["functions"][name] = method.__func__.__annotations__
+        return d
+
+    def __repr__(self):
+        return "<Schema for %s>" % self.klass
+
+@CaffeineClass
+class CaffeineService:
+    @CaffeinePublicMethod
+    @classmethod
+    def directory(self):
+        schemas = {}
+        for name,object in root_level_objects.items():
+            schemas[name] = Schema(object)
+        return schemas
 
 
--- testRPC.py
+++ testRPC.py
@@ -14,3 +14,23 @@ class TestSequence(unittest.TestCase):
     	self.assertEquals(Foo._caffeineRPC["helloWorld"].__func__,Foo.helloWorld.__func__)
     	self.assertEquals(RPC.root_level_objects["Foo"],Foo)
 
+    def test_schema(self):
+    	@RPC.CaffeineClass
+    	class Foo:
+    		@RPC.CaffeinePublicMethod
+    		@classmethod
+    		def stringLength(self,string:str) -> int:
+    			return len(string)
+
+    	args = []
+    	kwargs = {"string":"Test1"}
+    	print ("length is ",Foo.stringLength(*args,**kwargs))
+
+    	print (RPC.Schema(Foo)._caffeinePack())
+    	directory = RPC.CaffeineService.directory()
+    	fooPackedSchema = directory["Foo"]._caffeinePack()
+    	self.assertTrue(fooPackedSchema["functions"])
+
+
+
+
