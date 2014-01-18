7555858f836a25c594c181ff894f807e56a84c47
Drew Crawford <drew@sealedabstract.com>
2014-01-18 13:34:27 -0600

    spaces

--- testRPC.py
+++ testRPC.py
@@ -2,37 +2,37 @@ import RPC
 import unittest
 class TestSequence(unittest.TestCase):
     def setUp(self):
-    	pass
+        pass
     def test_decorate(self):
-    	@RPC.Class
-    	class Foo:
-    		@RPC.PublicMethod
-    		@classmethod
-    		def helloWorld(self):
-    			return "hello world"
-    	self.assertEquals(Foo.helloWorld(),"hello world")
-    	self.assertEquals(Foo._caffeineRPC["helloWorld"].__func__,Foo.helloWorld.__func__)
-    	self.assertEquals(RPC.root_level_objects["Foo"],Foo)
+        @RPC.Class
+        class Foo:
+            @RPC.PublicMethod
+            @classmethod
+            def helloWorld(self):
+                return "hello world"
+        self.assertEquals(Foo.helloWorld(),"hello world")
+        self.assertEquals(Foo._caffeineRPC["helloWorld"].__func__,Foo.helloWorld.__func__)
+        self.assertEquals(RPC.root_level_objects["Foo"],Foo)
 
     def test_schema(self):
-    	@RPC.Class
-    	class Foo:
-    		@RPC.PublicMethod
-    		@classmethod
-    		def stringLength(self,string:str) -> int:
-    			return len(string)
+        @RPC.Class
+        class Foo:
+            @RPC.PublicMethod
+            @classmethod
+            def stringLength(self,string:str) -> int:
+                return len(string)
 
-    	args = []
-    	kwargs = {"string":"Test1"}
-    	print ("length is ",Foo.stringLength(*args,**kwargs))
+        args = []
+        kwargs = {"string":"Test1"}
+        print ("length is ",Foo.stringLength(*args,**kwargs))
 
-    	print (RPC.Schema(Foo)._caffeinePack())
-    	directory = RPC.CaffeineService.directory()
-    	fooPackedSchema = directory["Foo"]._caffeinePack()
-    	self.assertTrue(fooPackedSchema["functions"])
+        print (RPC.Schema(Foo)._caffeinePack())
+        directory = RPC.CaffeineService.directory()
+        fooPackedSchema = directory["Foo"]._caffeinePack()
+        self.assertTrue(fooPackedSchema["functions"])
 
     def test_RPCCall(self):
-    	pass
+        pass
 
 
 
