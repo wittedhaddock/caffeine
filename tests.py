4b32a11bf2b43af9a4681855dc4cbf8630e31429
Drew Crawford <drew@sealedabstract.com>
2014-01-31 02:41:29 -0600

    coverage fix

--- tests.py
+++ tests.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     elif "--coverage" in sys.argv:
         import xmlrunner
         import os.path
-        if not os.path.exists('xunit-reports'): os.path.mkdir('xunit-reports')
-        runner = xmlrunner.XMLTestRunner(output='xunit-reports')
+        if not os.path.exists('/xunit-reports'): os.path.mkdir('/xunit-reports')
+        runner = xmlrunner.XMLTestRunner(output='/xunit-reports')
     else:
         runner = unittest.TextTestRunner()
     testsuite = unittest.TestLoader().discover('.')
